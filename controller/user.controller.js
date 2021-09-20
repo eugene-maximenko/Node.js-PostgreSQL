e4d72e2e754b4aa35b06d547e02ade38028c4101
Eugene Maximenko <zhek4770@gmail.com>
2021-09-20 12:15:40 +0300

    Init commit

---
+++ controller/user.controller.js
@@ -0,0 +1,23 @@
+class UserController {
+    async createUser(req, res) {
+
+    }
+
+    async getUser(res, res) {
+
+    }
+
+    async getOneUser(res, res) {
+
+    }
+
+    async updateUser(res, res) {
+
+    }
+
+    async deleteUser(res, res) {
+
+    }
+}
+
+module.exports = new UserController()
---
+++ routes/user.routes.js
@@ -0,0 +1,5 @@
+const Router = require('express')
+const router = new Router()
+const UserController = require('../controller/user.controller')
+
+module.exports = router
